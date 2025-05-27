e98e66fb2fcb693f0df0229a59f3fa73be9b089b
Rishitha1238 <141536317+rishitha2827@users.noreply.github.com>
2025-05-27 22:54:11 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 🔁 Use your deployed backend URL
-const API_BASE_URL = 'https://vercel.com/rishithas-projects-00318a93/gatepass-backend';
+const API_BASE_URL = 'https://gatepass-backend.vercel.app/';
 
 const API = axios.create({
   baseURL: `${API_BASE_URL}/api`,
